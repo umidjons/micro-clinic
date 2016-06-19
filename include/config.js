d871af8e87ffaeae6b3a2939ebab723dd6a6ff35
Umidjons <almatov.us@gmail.com>
2016-06-19 10:53:30 +0500

    Configured login & password to DB. Enabled authentication for mongodb.

--- include/config.js
+++ include/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     app_port: 3000,
-    db: 'mongodb://127.0.0.1:27017/starmed',
+    db: 'mongodb://admin:admin@127.0.0.1:27017/starmed',
     jwt: {
         expire: '24h',
         secret: 'TRprTfrXuwWVHZqLFiFNKwQhFbCGlopV15Th88RvwW8RJJN9v5bgrv03wWC0HQcoObHjzCvtb6M5JcmKNjDFYUvyhrCa3qMGyxQx'
--- startMongo.bat
+++ startMongo.bat
@@ -2,4 +2,4 @@
 SET MONGO_PATH=c:\Program Files\MongoDB\Server\3.2\bin\
 SET CONFIG_FILE=mongod.cfg
 CD /D %MONGO_PATH%
-mongod.exe --config %CONFIG_FILE%
+mongod.exe --config %CONFIG_FILE% -auth
